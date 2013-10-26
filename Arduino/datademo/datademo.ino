de55ab9a10c7988273c60c1235d660e7dfca1ca4
Mike Kotsch <mail@mikekotsch.de>
2013-10-26 18:00:22 +0200

    Connect to pressure sensor

--- Arduino/datademo/datademo.ino
+++ Arduino/datademo/datademo.ino
@@ -1,15 +1,82 @@
-int sensorPin = A0;    // analog input pin to hook the sensor to
-int sensorValue = 0;  // variable to store the value coming from the sensor
-int potentiometer = 0; 
-
-void setup() {
-  Serial.begin(9600); // initialize serial communications
-  pinMode(potentiometer, INPUT);
-}
+/*
+ MPL3115A2 Barometric Pressure Sensor Library Example Code
+ By: Nathan Seidle
+ SparkFun Electronics
+ Date: September 24th, 2013
+ License: This code is public domain but you buy me a beer if you use this and we meet someday (Beerware license).
  
-void loop() {
-  int value = analogRead(potentiometer);
-  //Serial.println(value); //pirnt the value on the serial monitor screen
-  Serial.write(value); // print bytes to serial
-  delay(100);
+ Uses the MPL3115A2 library to display the current altitude and temperature
+ 
+ Hardware Connections (Breakoutboard to Arduino):
+ -VCC = 3.3V
+ -SDA = A4 (use inline 10k resistor if your board is 5V)
+ -SCL = A5 (use inline 10k resistor if your board is 5V)
+ -INT pins can be left unconnected for this demo
+ 
+ During testing, GPS with 9 satellites reported 5393ft, sensor reported 5360ft (delta of 33ft). Very close!
+ During testing, GPS with 8 satellites reported 1031ft, sensor reported 1021ft (delta of 10ft).
+ 
+ Available functions:
+ .begin() Gets sensor on the I2C bus.
+ .readAltitude() Returns float with meters above sealevel. Ex: 1638.94
+ .readAltitudeFt() Returns float with feet above sealevel. Ex: 5376.68
+ .readPressure() Returns float with barometric pressure in Pa. Ex: 83351.25
+ .readTemp() Returns float with current temperature in Celsius. Ex: 23.37
+ .readTempF() Returns float with current temperature in Fahrenheit. Ex: 73.96
+ .setModeBarometer() Puts the sensor into Pascal measurement mode.
+ .setModeAltimeter() Puts the sensor into altimetery mode.
+ .setModeStandy() Puts the sensor into Standby mode. Required when changing CTRL1 register.
+ .setModeActive() Start taking measurements!
+ .setOversampleRate(byte) Sets the # of samples from 1 to 128. See datasheet.
+ .enableEventFlags() Sets the fundamental event flags. Required during setup.
+ 
+*/
+
+#include <Wire.h>
+#include "MPL3115A2.h"
+
+const char HEADER = 'H';  // a single character header to indicate 
+                          // the start of a message
+
+//Create an instance of the object
+MPL3115A2 myPressure;
+
+void setup()
+{
+  Wire.begin();        // Join i2c bus
+  Serial.begin(9600);  // Start serial for output
+
+  myPressure.begin(); // Get sensor online
+
+  // Configure the sensor
+  //myPressure.setModeAltimeter(); // Measure altitude above sea level in meters
+  myPressure.setModeBarometer(); // Measure pressure in Pascals from 20 to 110 kPa
+  
+  myPressure.setOversampleRate(7); // Set Oversample to the recommended 128
+  myPressure.enableEventFlags(); // Enable all three pressure and temp event flags 
+}
+
+void loop()
+{
+  
+  Serial.write(HEADER);
+  
+  float pressure = myPressure.readPressure();
+  Serial.print("Pressure(Pa):");
+//  Serial.print(pressure, 2);
+  
+  Serial.print((int)pressure);
+  
+  sendBinary((int) pressure);
+  
+  Serial.println();
+  
+  delay(10);
+}
+
+void sendBinary( int value)
+{
+  // send the two bytes that comprise an integer
+  Serial.write(lowByte(value));  // send the low byte
+  Serial.write(highByte(value)); // send the high byte
 }
